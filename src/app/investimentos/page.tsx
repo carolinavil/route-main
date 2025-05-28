29b7a94ca184aabff864c1e7d8950c5068cfd3f7
luanadias00 <luanagarrido622@gmail.com>
2025-05-28 09:39:09 -0300

    22

--- src/app/investimentos/page.tsx
+++ src/app/investimentos/page.tsx
@@ -5,16 +5,19 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Investimentos | Route Investimentos",
-  description: "Soluções oferecidas em parceria com a Safra",
+  description: "Soluções oferecidas em parceria com a Safra.",
   // other metadata
 };
 
 const Compliance = () => {
+  
+  console.log("Componente de Investimentos renderizado");
+  
   return (
     <>
       <Breadcrumb
         pageName="Investimentos"
-          description="Soluções oferecidas pela assessoria vinculada à Safra"
+          description="Soluções oferecidas pela assessoria vinculada à Safra."
       />
       <InvestSectionTwo />
     </>
